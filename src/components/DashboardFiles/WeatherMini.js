5bc2e5d2544d4f78c5fdc4b0fa2aacbc801e9739
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2023-01-05 23:28:21 -0600

    just updating

---
+++ src/components/DashboardFiles/WeatherMini.js
@@ -0,0 +1,54 @@
+import { useState, useEffect, useCallback } from 'react'
+import axios from 'axios'
+import { useNavigate, Link } from 'react-router-dom'
+
+// This will be for the little bit of info on the dashboard
+const WeatherMini = (props) => {
+    
+    const [icon, setIcon] = useState()
+    const [temp, setTemp] = useState()
+    const [wData, setWData] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+
+    const weatherData = []
+    const navigate = useNavigate()
+
+    const getAxios = useCallback( async (city) => {
+        await axios.get('https://weather-app-eevee.herokuapp.com/api/info/' + city.city)
+        .then((res) => {
+            setWData(res.data)
+            setTemp(res.data.main.temp)
+            setIcon(res.data.weather[0].icon)
+
+        },
+        
+        (err) => console.log(err))
+    })
+    
+    const getData = () => {    
+        if (isLoading) {
+            return <h1>Loading...</h1>
+        } 
+        setTimeout(getData, 3000)
+        return <h1>{wData.main.temp}</h1>
+    }
+
+    useEffect(()=> {
+        // I will try to use the city name to get data from each item in userList from dashboard
+        getAxios(props.userCityNotes)
+        
+    }, [])
+
+    return (
+        <>
+        <div className="mx-5 mb-4">
+            <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`}/>
+            <h1>{temp} °F</h1>
+
+        </div>
+        
+        </>
+    )
+}
+
+export default WeatherMini;
